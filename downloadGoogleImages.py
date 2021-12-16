1d1506c8a8be2638184900ed677fba9ec586b4a8
Darsh09 <61144167+Darsh09@users.noreply.github.com>
2021-12-16 17:25:33 +0530

    Add files via upload

--- downloadGoogleImages.py
+++ downloadGoogleImages.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
 
@@ -10,7 +10,7 @@ CORS(app)
 
 from google_images_download import google_images_download
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['POST'])
 def main():
     
     search_query=request.json['search']
@@ -18,39 +18,40 @@ def main():
     
     response = google_images_download.googleimagesdownload()
     
-    def downloadimages(query):
+    # def downloadimages(query):
         
-        arguments = {"keywords": search_query,
-                    "format": "jpg",
-                    "limit":numberOfImages,
-                    "print_urls":True,
-                    "size": "medium",
-                    "aspect_ratio":"panoramic"}
-        try:
-            response.download(arguments)
+    arguments = {"keywords": search_query,
+                "format": "jpg",
+                "limit":numberOfImages,
+                "print_urls":True,
+                "size": "medium",
+                "aspect_ratio":"panoramic"}
+    # try:
+    response.download(arguments)
+    return jsonify({'msg': 'Successful'})
 
-        # Handling File NotFound Error	
-        except FileNotFoundError:
-            arguments = {"keywords": query,
-                        "format": "jpg",
-                        "limit":4,
-                        "print_urls":True,
-                        "size": "medium"}
+        # # Handling File NotFound Error	
+        # except FileNotFoundError:
+        #     arguments = {"keywords": query,
+        #                 "format": "jpg",
+        #                 "limit":4,
+        #                 "print_urls":True,
+        #                 "size": "medium"}
                         
-            # Providing arguments for the searched query
-            try:
-                # Downloading the photos based
-                # on the given arguments
-                response.download(arguments)
-                return 200
-            except:
-                pass
-                return 500
+        #     # Providing arguments for the searched query
+        #     try:
+        #         # Downloading the photos based
+        #         # on the given arguments
+        #         response.download(arguments)
+        #         return 200
+        #     except:
+        #         pass
+        #         return 500
 
             
     # Driver Code
 
-    downloadimages(search_query)
+    # downloadimages(search_query)
     # print()
 
 if __name__ == '__main__':
