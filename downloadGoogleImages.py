cbd33a5f40bd69f2b4ca05a440dacc9e092bf35b
Darsh09 <61144167+Darsh09@users.noreply.github.com>
2021-12-16 16:22:29 +0530

    Add files via upload

---
+++ downloadGoogleImages.py
@@ -0,0 +1,57 @@
+from flask import Flask, request
+from flask_cors import CORS
+import requests
+
+app = Flask(__name__)
+CORS(app)
+
+# search_query = input('Enter search query')
+# numberOfImages  = int(input('Number of Images'))
+
+from google_images_download import google_images_download
+
+@app.route('/', methods=['POST'])
+def main():
+    
+    search_query=request.json['search']
+    numberOfImages=request.json['number']
+    
+    response = google_images_download.googleimagesdownload()
+    
+    def downloadimages(query):
+        
+        arguments = {"keywords": search_query,
+                    "format": "jpg",
+                    "limit":numberOfImages,
+                    "print_urls":True,
+                    "size": "medium",
+                    "aspect_ratio":"panoramic"}
+        try:
+            response.download(arguments)
+
+        # Handling File NotFound Error	
+        except FileNotFoundError:
+            arguments = {"keywords": query,
+                        "format": "jpg",
+                        "limit":4,
+                        "print_urls":True,
+                        "size": "medium"}
+                        
+            # Providing arguments for the searched query
+            try:
+                # Downloading the photos based
+                # on the given arguments
+                response.download(arguments)
+                return 200
+            except:
+                pass
+                return 500
+
+            
+    # Driver Code
+
+    downloadimages(search_query)
+    # print()
+
+if __name__ == '__main__':
+    app.run(debug=True)
