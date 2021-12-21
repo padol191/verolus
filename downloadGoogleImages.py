52ea35aa3e2d7479831a8672fc4d277154ddd8b3
chaitanya <chaitanyapadol@icloud.com>
2021-12-21 14:36:29 +0530

    updated

--- downloadGoogleImages.py
+++ downloadGoogleImages.py
@@ -1,3 +1,4 @@
+from google_images_download import google_images_download
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import requests
@@ -9,77 +10,81 @@ CORS(app)
 # search_query = input('Enter search query')
 # numberOfImages  = int(input('Number of Images'))
 
-from google_images_download import google_images_download
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
+
 @app.route('/google', methods=['GET'])
 def google():
     return render_template('google.html')
+
 
 @app.route('/instagram', methods=['GET'])
 def instagram():
     return render_template('instagram.html')
 
+
 @app.route('/website', methods=['GET'])
 def website():
     return render_template('website.html')
+
 
 @app.route('/classes', methods=['GET'])
 def classes():
     return render_template('classes.html')
 
+
 @app.route('/search', methods=['POST'])
 def main():
-    
-    search_query=request.json['search']
-    numberOfImages=request.json['number']
-    
+
+    search_query = request.json['search']
+    numberOfImages = request.json['number']
+
     response = google_images_download.googleimagesdownload()
-    
+
     # def downloadimages(query):
-        
+
     arguments = {"keywords": search_query,
-                "format": "jpg",
-                "limit":numberOfImages,
-                "print_urls":True,
-                "size": "medium",
-                "aspect_ratio":"panoramic",
-                "output_directory": os.path.join(os.path.join(os.environ['HOME']), 'Desktop')}
+                 "format": "jpg",
+                 "limit": numberOfImages,
+                 "print_urls": True,
+                 "size": "medium",
+                 "aspect_ratio": "panoramic",
+                 }
     # try:
     response.download(arguments)
     return render_template('google.html')
 
-        # # Handling File NotFound Error	
-        # except FileNotFoundError:
-        #     arguments = {"keywords": query,
-        #                 "format": "jpg",
-        #                 "limit":4,
-        #                 "print_urls":True,
-        #                 "size": "medium"}
-                        
-        #     # Providing arguments for the searched query
-        #     try:
-        #         # Downloading the photos based
-        #         # on the given arguments
-        #         response.download(arguments)
-        #         return 200
-        #     except:
-        #         pass
-        #         return 500
+    # # Handling File NotFound Error
+    # except FileNotFoundError:
+    #     arguments = {"keywords": query,
+    #                 "format": "jpg",
+    #                 "limit":4,
+    #                 "print_urls":True,
+    #                 "size": "medium"}
 
-            
+    #     # Providing arguments for the searched query
+    #     try:
+    #         # Downloading the photos based
+    #         # on the given arguments
+    #         response.download(arguments)
+    #         return 200
+    #     except:
+    #         pass
+    #         return 500
+
     # Driver Code
 
     # downloadimages(search_query)
     # print()
-    
+
 # @app.route('/instasearch', methods=['POST'])
 # def instasearch():
-    
+
 #     return NoneType
+
 
 if __name__ == '__main__':
     app.run(debug=False)
